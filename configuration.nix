44258bd7e903a8bd6db021ab8c095caa541192a8
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2024-04-22 15:50:26 +0000

    grub

--- configuration.nix
+++ configuration.nix
@@ -1,11 +1,13 @@
 { pkgs, ... }:
 {
   imports = [
-    ./disko.nix
+    ./disk-config.nix
     ./shared.nix
   ];
 
-  boot.loader = {
+  boot.loader.grub = {
+    # no need to set devices, disko will add all devices that have a EF02 partition to the list already
+    # devices = [ ];
     efiSupport = true;
     efiInstallAsRemovable = true;
   };
