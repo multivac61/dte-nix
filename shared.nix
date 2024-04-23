b40c08cbb2ca458382bbec6b477cb50a30e0c78d
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2024-04-23 18:16:58 +0000

    Don't need any overlays

--- shared.nix
+++ shared.nix
@@ -7,15 +7,6 @@
       allowInsecure = false;
       allowUnsupportedSystem = true;
     };
-
-    # # Apply each overlay found in the /overlays directory
-    # overlays =
-    #   let path = ../../overlays; in with builtins;
-    #   map (n: import (path + ("/" + n)))
-    #     (filter
-    #       (n: match ".*\\.nix" n != null ||
-    #         pathExists (path + ("/" + n + "/default.nix")))
-    #       (attrNames (readDir path)));
   };
 
   nix = {
