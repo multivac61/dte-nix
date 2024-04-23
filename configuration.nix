176bd84b0e8632c499393ca12a4ddb2834a97959
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2024-04-23 19:04:16 +0000

    Add cache substituters

--- configuration.nix
+++ configuration.nix
@@ -46,6 +46,19 @@
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGqWL96+z6Wk2IgF6XRyoZAVUXmCmP8I78dUpA4Qy4bh genki@gdrn"
   ];
 
+  nix.settings = {
+    substituters = [
+      "https://genki.cachix.org" # In the future this could be DTE's own cache
+      "https://nix-community.cachix.org"
+      "https://cache.nixos.org"
+    ];
+    trusted-public-keys = [
+      "genki.cachix.org-1:5l+wAa4rDwhcd5Wm43eK4N73qJ6GIKmJQ87Nw/bRGfE="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+    ];
+  };
+
   nixpkgs.config.allowUnfree = true;
 
   environment.systemPackages = with pkgs; [
@@ -53,6 +66,7 @@
     git
     gh
     neofetch
+    cachix
     # other packages...
   ];
 
