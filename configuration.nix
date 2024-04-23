2358a381d44d34a5f7f1ee7ef9afdbd4aafff4aa
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2024-04-23 18:31:51 +0000

    Add postgresql

--- configuration.nix
+++ configuration.nix
@@ -9,6 +9,14 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
+  services.postgresql.enable = true;
+  services.postgresql.package = pkgs.postgresql_14;
+  services.postgresql.settings = {
+    max_connections = "300";
+    shared_buffers = "80MB";
+  };
+  services.postgresqlBackup.enable = true;
+
   networking.hostName = "joip";
   users = {
     mutableUsers = false;
